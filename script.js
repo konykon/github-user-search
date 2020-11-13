47d0b083257d310d5c44689fcfdd765559765cd8
konykon <tsagarikon@hotmail.com>
2020-11-13 13:01:51 +0100

    refactored script, arrow functions and async await

--- script.js
+++ script.js
@@ -1,86 +1,51 @@
-var userData;
-var reposData;
-
-function apiCall() {
-	var site = "https://api.github.com/users/";
-	var user = document.getElementById("search").value;
-	var userUrl = site + user;
-	getUserData(userUrl);
-}
-
-var searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", function () {
-	apiCall();
-	document.getElementById('search').value = "";
+$("#search-button").click(() => {
+	displayContent().catch((e) => {
+		$("#exist").hide();
+		$("#notExist").show();
+	});
+	$('#search').val("");
 });
 
-function getUserData(pageUrl) {
-	fetch(pageUrl, {
+const get = async (url) => {
+	let response = await fetch(url, {
 		method: "GET",
-	}).then(function (response) {
-		if (response.ok) {
-			return response.json();
-		} else {
-			showDiv('notExist', 'exist');
-		}
-		throw new Error(response.statusText);
-	}).then(function (json) {
-		userData = json;
-		generateUserInfo(userData);
-		getReposData(userData.repos_url);
-		showDiv('exist', 'notExist');
-	}).catch(function (error) {
-		console.log("Request failed: " + error.message);
 	});
-}
-
-function getReposData(pageUrl) {
-	fetch(pageUrl, {
-		method: "GET",
-	}).then(function (response) {
-		if (response.ok) {
-			return response.json();
-		}
-		throw new Error(response.statusText);
-	}).then(function (json) {
-		reposData = json;
-		generateReposInfo(reposData);
-	}).catch(function (error) {
-	});
-}
-
-function generateUserInfo(dataObj) {
-	var avatar = document.getElementById("avatar");
-	var userName = document.getElementById("userName");
-	var name = document.getElementById("name");
-	var bio = document.getElementById("bio");
-	avatar.setAttribute("src", dataObj.avatar_url);
-	userName.innerHTML = '@' + dataObj.login;
-	name.innerHTML = dataObj.name;
-	bio.innerHTML = dataObj.bio;
-}
-
-function generateReposInfo(dataArr) {
-	var list = document.getElementById("list");
-	list.innerHTML = " ";
-	if (dataArr.length > 0) {
-		for (var i = 0; i < dataArr.length; i++) {
-			var li = document.createElement('li');
-			list.appendChild(li);
-			li.innerHTML = '<div class="repo-name">' + dataArr[i].name + '</div><span class="span"><i class="fas fa-star"></i> ' + dataArr[i].stargazers_count + ' <i class="fas fa-code-branch"></i> ' + dataArr[i].forks + '</span>';
-		}
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
 	} else {
-		li = document.createElement('li');
-		list.appendChild(li);
-		li.innerHTML = "<span class='noRepo'>This user has no repositories</span>";
+		return await response.json();
 	}
 }
 
-function showDiv(ID, Id) {
-	var DivToShow = document.getElementById(ID);
-	var DivToHide = document.getElementById(Id);
-	DivToShow.classList.remove("hidden");
-	DivToShow.classList.add("visible");
-	DivToHide.classList.remove("visible");
-	DivToHide.classList.add("hidden");
+const displayContent = async () => {
+	let user = $("#search").val();
+	const urlUser = get(`https://api.github.com/users/${user}`);
+	const urlRepo = get(`https://api.github.com/users/${user}/repos`);
+
+	let urls = await Promise.all([urlUser, urlRepo]);
+
+	generateUserData(urls[0]);
+	generateReposData(urls[1]);
+	$("#notExist").hide();
+	$("#exist").show();
+}
+
+const generateUserData = (userData) => {
+	$("#avatar").attr("src", userData.avatar_url);
+	$("#userName").text('@' + userData.login);
+	$("#name").text(userData.name);
+	$("#bio").text(userData.bio);
+}
+
+const generateReposData = (dataArr) => {
+	$("#list").html("");
+	if (dataArr.length > 0) {
+		dataArr.forEach(data => {
+			let $li = $("<li></li>").appendTo("#list");
+			$li.html(`${data.name}<span><i class="fas fa-star"></i>${data.stargazers_count}<i class="fas fa-code-branch"></i>${data.forks}</span>`);
+		});
+	} else {
+		let $li = $("<li></li>").appendTo("#list");
+		$li.html("<span class='noRepo'>This user has no repositories</span>");
+	}
 }
